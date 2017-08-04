60fb2d97005c609f6d20cbf3a9564cec0d6e797c
carlmagumpara <magumparacarlanthony@gmail.com>
2017-08-04 17:24:51 +0800

    something

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,7 +28,7 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
                 <div class="panel-body">
-                   <h2>Hi! {{ Auth::user()->name }}</h2>
+                    <h2>Hi! {{ Auth::user()->name }}</h2>
                 </div>
             </div>
         </div>
